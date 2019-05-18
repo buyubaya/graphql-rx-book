8252a66040393c62cccd7007419a561eafae6ca0
buyubaya <hihihi_caigikia@yahoo.com>
2019-05-18 17:07:00 +0700

    Updates

---
+++ precache-manifest.4c85e294067fd978cb6b4638541eb088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8702ff692c892e626638",
+    "url": "/graphql-rx-book/static/js/runtime~main.8702ff69.js"
+  },
+  {
+    "revision": "c7c0947b083cc2a4558f",
+    "url": "/graphql-rx-book/static/js/main.c7c0947b.chunk.js"
+  },
+  {
+    "revision": "d3c0334d90638b40e55e",
+    "url": "/graphql-rx-book/static/js/1.d3c0334d.chunk.js"
+  },
+  {
+    "revision": "c7c0947b083cc2a4558f",
+    "url": "/graphql-rx-book/static/css/main.ba1dcfd2.chunk.css"
+  },
+  {
+    "revision": "059575f7b8bdcd4fbcfaa792944723fa",
+    "url": "/graphql-rx-book/index.html"
+  }
+];
